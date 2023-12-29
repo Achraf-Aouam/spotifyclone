fceaad4e4074521e604a65383383f4dea964113f
Achraf-Aouam <a.aouam@aui.ma>
2023-12-29 16:35:51 +0000

    Update dependencies and styles

---
+++ components/Button.tsx
@@ -0,0 +1,16 @@
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button type={type} className={twMerge("w-full rounded-full ")}></button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
