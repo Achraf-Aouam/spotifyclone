47965c204e12ad056c6701966e520c9b2f8b7468
Achraf-Aouam <a.aouam@aui.ma>
2024-01-01 21:48:34 +0100

    Add getSongs import

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["omzvjlpwyaljjqlkmriu.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
