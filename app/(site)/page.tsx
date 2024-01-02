4b7a93c21396015830d4b4f820838ea3102f9925
Achraf-Aouam <a.aouam@aui.ma>
2024-01-02 13:03:51 +0100

    small update

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,19 +11,23 @@ export default async function Home() {
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
         <div className="mb-2">
-          <h1 className="text-white text-3xl font-semibold">welcome back</h1>
+          <h1 className="text-white text-3xl font-semibold">
+            welcome back MFs
+          </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4  ">
             <ListItem
               href="liked"
               image="/images/liked.png"
-              name="Liked Songs"
+              name="diskat li 3jbok"
             />
           </div>
         </div>
       </Header>
       <div className="mt-2 mb-7 px-6">
         <div className="flex justify-between items-center">
-          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+          <h1 className="text-white text-2xl font-semibold">
+            akhir makayn f diskat
+          </h1>
         </div>
         <PageContent songs={songs} />
       </div>
