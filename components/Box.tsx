7a4b795a930daf836e0906722cb3af5e9a9c43ed
Achraf-Aouam <a.aouam@aui.ma>
2024-01-02 12:49:00 +0100

    semi-final

--- components/Box.tsx
+++ components/Box.tsx
@@ -2,13 +2,13 @@ import { twMerge } from "tailwind-merge";
 
 interface BoxProps {
   children: React.ReactNode;
-  clasName?: string;
+  className?: string;
 }
 
-const Box: React.FC<BoxProps> = ({ children, clasName }) => {
+const Box: React.FC<BoxProps> = ({ children, className }) => {
   return (
     <div
-      className={twMerge("bg-neutral-900 rounded-lg h-fit w-full  ", clasName)}
+      className={twMerge("bg-neutral-900 rounded-lg h-fit w-full  ", className)}
     >
       {children}
     </div>
--- components/LIbrary.tsx
+++ components/LIbrary.tsx
@@ -5,10 +5,20 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const { user } = useUser();
+
+  const onplay = useOnPlay(songs);
+
   const uploadModal = useUploadModal();
 
   const onClick = () => {
@@ -32,7 +42,15 @@ const Library = () => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3"> List of Songs!</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          <MediaItem
+            onClick={(id: string) => onplay(id)}
+            key={item.id}
+            data={item}
+          />
+        ))}
+      </div>
     </div>
   );
 };
